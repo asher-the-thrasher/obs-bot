cee8dc31543a6aaeffb16e8bbd67606f5157bfb2
asher-the-thrasher <76796294+asher-the-thrasher@users.noreply.github.com>
2022-04-27 16:24:53 -0400

    this is what i use to run the bot

---
+++ my_setup/runningcommand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3.8 runner.py -c /Users/ashersasson/Documents/asherthethrasher/obs-bot/my_setup/config.toml -l /Users/ashersasson/Documents/asherthethrasher/obs-bot/my_setup/logs/log.txt
